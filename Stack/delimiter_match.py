417bb68dffda4968ee730afddf58bbc1907a862f
Ayush Gupta <atulg7516@gmail.com>
2018-06-08 15:29:50 +0530

    Create delimiter_match.py

---
+++ Stack/delimiter_match.py
@@ -0,0 +1,16 @@
+'''
+A delimiter is a sequence of one or more characters used to specify the boundary
+between separate, independent regions in plain text or other data streams.
+( )	Parentheses
+{ }	Braces (also called curly brackets)
+[ ]	Brackets
+< >	Angle brackets
+" "	commonly used to denote string literals.
+' '	commonly used to denote character literals.
+'''
+import stack
+def d_match(text):
+    left ='({[<''
+    right = ')}]>'
+    for i in text:
+        if i in
