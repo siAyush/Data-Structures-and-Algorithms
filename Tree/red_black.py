4164211f439b82da54ea9661c546d5692a3b91f8
siAyush <atulg7516@gmail.com>
2018-08-10 17:17:45 +0530

    need changes in violation

--- Tree/red_black.py
+++ Tree/red_black.py
@@ -22,27 +22,37 @@ class Red_Black():
     def _in_order_traverse(self,node):
         if node.left:
             self._in_order_traverse(node.left)
-        print('Node data %s'%node.data)
+        print('Node data %s Node color %s'%(node.data ,node.color))
         if node.right:
             self._in_order_traverse(node.right)
 
-    def parent(self,node):       # return the parent of node
+    def parent(self,node):        # return the parent of node
         return node.parent
 
     def set_black(self,node):    # change the default color of node
         node.color = Black
 
     def uncle(self,node):
-        parent = self.parent(node)
-        grand_parent = self.parent(parent)
-        if parent.data < grand_parent.data:
-            return grand_parent.right
-        if parent.data > grand_parent.data:
-            return grand_parent.left
+        if node:
+            if self.parent(node):
+                parent = self.parent(node)
+            if self.parent(node) == None:
+                parent = None
+            if parent is not None:
+                grand_parent = self.parent(parent)
+            if parent == None:
+                grand_parent = None
+            if parent and grand_parent is not None:
+                if parent.data < grand_parent.data:
+                    return grand_parent.right
+                if parent.data > grand_parent.data:
+                    return grand_parent.left
+        return None
 
     def insert(self,data):
         if not self.root:
             self.root = Node(data,None)
+            self.set_black(self.root)
         self._insertNode(data,self.root)
 
     def _insertNode(self,data,node):
@@ -51,39 +61,41 @@ class Red_Black():
                 self._insertNode(data,node.left)
             else:
                 node.left = Node(data,node)
-
-
         if data > node.data:
             if node.right:
                 self._insertNode(data,node.right)
             else:
                 node.right = Node(data,node)
-
         return self.violation(data,node)
 
     def violation(self,data,node):
         uncle = self.uncle(node)
         parent = self.parent(node)
-        if self.parent(parent):
+        if parent is None:
+            grand_parent = None
+        if parent:
             grand_parent = self.parent(parent)
 
         # Case I (when uncle and node is red)
-        if node.data < grand_parent.data and grand_parent.parent == None and uncle.color == Red:
-            grand_parent.color = Black
-            grand_parent.left.color = Black
-            grand_parent.right.color = Black
-            return grand_parent
+        if node and grand_parent:
+            if node.data < grand_parent.data and grand_parent.parent == None and uncle.color == Red:
+                '''grand_parent.color = Black
+                grand_parent.left.color = Black
+                grand_parent.right.color = Black'''
+                self.set_black(grand_parent)
+                self.set_black(grand_parent.left)
+                self.set_black(grand_parent.right)
+                return grand_parent
 
-        elif node.data < grand_parent.data and uncle.color == Red:
-            grand_parent.color = Red
-            grand_parent.left.color = Black
-            grand_parent.right.color = Black
-            return grand_parent
+            elif node.data < grand_parent.data and uncle.color == Red:
+                grand_parent.color = Red
+                grand_parent.left.color = Black
+                grand_parent.right.color = Black
+                return grand_parent
 
 a = Red_Black()
 a.insert(10)
 a.insert(20)
 a.insert(5)
 a.insert(7)
-#print(a.parent(a.root.left).data)
-print(0x00000240D16FC240)
+a.in_order_traverse()
