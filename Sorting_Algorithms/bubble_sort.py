44440434d8346c1471b47e6eb1bda258366eb6e5
siAyush <atulg7516@gmail.com>
2018-08-16 18:55:53 +0530

    add docs

--- Sorting_Algorithms/bubble_sort.py
+++ Sorting_Algorithms/bubble_sort.py
@@ -1,1 +1,12 @@
 
+def bubble_sort(array):
+    for i in range(len(array)):
+        for j in range(len(array)-i-1):
+            if array[j] > array[j+1]:
+                array[j],array[j+1] = array[j+1],array[j]
+    return array
+
+
+a = [i for i in reversed(range(500))]
+bubble_sort(a)
+print(a)
