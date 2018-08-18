8a92564a71446332a0c3c99089a96700e3df81b5
siAyush <atulg7516@gmail.com>
2018-08-18 15:39:00 +0530

    Bubble sort

--- Sorting_Algorithms/bubble_sort.py
+++ Sorting_Algorithms/bubble_sort.py
@@ -1,12 +1,13 @@
-
+'''
+Bubble Sort -->
+Bubble Sort is a comparison based sorting algorithm.It compares each pair of
+adjacent items and swap them if  they are in the wrong order.It is in-place and
+stable sorting algorithm and no need of extra memory for sorting.
+Time Complexity --- O(N^2)
+'''
 def bubble_sort(array):
     for i in range(len(array)):
         for j in range(len(array)-i-1):
             if array[j] > array[j+1]:
                 array[j],array[j+1] = array[j+1],array[j]
     return array
-
-
-a = [i for i in reversed(range(500))]
-bubble_sort(a)
-print(a)
