664e23a05ffc507e41f867ccfd49e5d040b7b59d
siAyush <atulg7516@gmail.com>
2018-08-19 12:07:08 +0530

    selection sort

--- Sorting_Algorithms/selection_sort.py
+++ Sorting_Algorithms/selection_sort.py
@@ -1,1 +1,16 @@
+'''
+Selection Sort is a O(N^2) complexity algorithm.
+This algorithm proceeds by finding the smallest item in the
+unsorted array.And then swap it with the leftmost unsorted
+element.It doesn't need extra memory for sortingself.
+'''
 
+def selection_sort(array):
+    for i in range(len(array)-1):
+        index = i
+        for j in range(i+1,len(array)):
+            if array[j] < array[index]:
+                index = j
+        if index != i:
+            array[index],array[i] = array[i],array[index]
+    return array
