2930eca68496bfebed710ca3fd224c57c4bfdc79
siAyush <atulg7516@gmail.com>
2018-09-04 18:30:16 +0530

    merge sort

--- Sorting_Algorithms/merge_sort.py
+++ Sorting_Algorithms/merge_sort.py
@@ -1,1 +1,32 @@
-
+'''
+Merge Sort is a divide and conqure algorithm.It's a comparison based algorithm
+with time complexity O(N-logN).It's not a inplace algorithm and need additional
+memory for sorting.
+'''
+def merge_sort(array):
+    if len(array) == 1:
+        return None
+    middel_index = len(array)//2
+    left_array = array[:middel_index]
+    right_array = array[middel_index:]
+    merge_sort(left_array)
+    merge_sort(right_array)
+    i = 0                  # left sub array index
+    j = 0                  # right sub array index
+    k = 0                  # merged array index
+    while i < len(left_array) and j < len(right_array):
+        if left_array[i] < right_array[j]:
+            array[k] = left_array[i]
+            i += 1
+        else:
+            array[k] = right_array[j]
+            j += 1
+        k += 1
+    while i < len(left_array):
+        array[k] = left_array[i]
+        i += 1
+        k += 1
+    while j < len(right_array):
+        array[k] = right_array[j]
+        j +=1
+        k += 1
