d296ecd8b23093eeb56ebb8ccf81bbb253aae913
siAyush <atulg7516@gmail.com>
2018-09-08 19:31:11 +0530

    brute_force_search

--- Pattern_Matching_Algorithms/brute_force_search.py
+++ Pattern_Matching_Algorithms/brute_force_search.py
@@ -1,1 +1,18 @@
+'''
+Brute Force Search enumerate all possible configurations of inputs involved
+and picks the best(pattern matched) of all these enumerated configurationsself.
 
+Keep iteration through the text and if there is mismatch we shift the pattern
+one step to the right.
+'''
+
+def brute_force_search(text,pattern):
+    n = len(text)           # length of text
+    m = len(pattern)        # length of pattern
+    for i in range(n-m+1):
+        k = 0               # an index into pattern
+        while k < m and text[i+k] == pattern[k]:
+            k += 1
+        if k == m:
+            return i
+    return None            # failed to find match 
