578592bfc8b5013cb45539a0f48b62633883d7b9
siAyush <atulg7516@gmail.com>
2018-09-13 16:50:58 +0530

    Boyer–Moore algorithm

--- Pattern_Matching_Algorithms/boyer_moore.py
+++ Pattern_Matching_Algorithms/boyer_moore.py
@@ -1,3 +1,14 @@
+'''
+Boyer–Moore string-search algorithm is an efficient string-searching algorithm
+that is the standard benchmark for practical string-search literature.The
+algorithm preprocesses the string being searched for (the pattern), but not
+the string being searched in (the text).The Boyer-Moore algorithm uses
+information gathered during the preprocess step to skip sections of the text, 
+resulting in a lower constant factor than many other string search algorithms.
+The key features of the algorithm are to match on the tail of the pattern
+rather than the head, and to skip along the text in jumps of multiple
+ characters rather than searching every single character in the text.
+'''
 def boyer_moore(text,pattern):
     n = len(text)                         # lenght of text
     m = len(pattern)                      # length of pattern
@@ -16,10 +27,7 @@ def boyer_moore(text,pattern):
                 t -= 1
                 p -= 1
         else:
-            if last.get(text[t]):
-                t = last.get(text[t]) 
+            j = last.get(text[t],-1)
+            t += m-min(p,j+1)
             p = m-1
     return None
-a = 'this is a test'
-b = 'test'
-print(boyer_moore(a,b))
