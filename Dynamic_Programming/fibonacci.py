32c6a6bda92a03a32a2adab838c91977c2f732a0
Ayush <atulg7516@gmail.com>
2018-10-09 18:54:05 +0530

    dynamic_programming

--- Dynamic_Programming/fibonacci.py
+++ Dynamic_Programming/fibonacci.py
@@ -1,2 +1,13 @@
+def fib(n,memo):
+    if memo[n] != None:
+        return memo[n]
+    if n == 1 or n == 2:
+        return 1
+    else:
+        result = fib(n-1,memo)+fib(n-2,memo)
+    memo[n] = result
+    return result
 
-def fib()
+def fib_memo(n):
+    memo = [None]*int(n+1)
+    return fib(n,memo)
