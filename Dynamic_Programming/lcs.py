c5aa5929ee10e9e9b117bbe99a38d8af72881a44
Ayush <atulg7516@gmail.com>
2018-10-10 11:40:01 +0530

    lcs

--- Dynamic_Programming/lcs.py
+++ Dynamic_Programming/lcs.py
@@ -1,1 +1,22 @@
+"""
+Longest Common Subsequence (lcs) using dynamic programming.
+"""
 
+data = [[None]*4 for i in range(3)]
+
+
+def lcs(p, q, n, m):
+    if data[n][m] is not None:
+        return data[n][m]
+
+    if n == 0 or m == 0:
+        result = 0
+
+    elif p[n-1] == q[m-1]:
+        result = 1+lcs(p, q, n-1, m-1)
+
+    elif p[n-1] != q[m-1]:
+        temp1 = lcs(p, q, n-1, m)
+        temp2 = lcs(p, q, n, m-1)
+        result = max(temp1, temp2)
+    return result
