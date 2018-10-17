900c4757076e353f6c1d2e0f21930bc3e065f368
Ayush <atulg7516@gmail.com>
2018-10-17 13:56:56 +0530

    TST

--- Tries/tst.py
+++ Tries/tst.py
@@ -26,16 +26,29 @@ class TST():
             node = Node(c)
         if c < node.character:
             node.left = self.insert_node(node.left,key,value,index)
-        if c > node.character:
+        elif c > node.character:
             node.right = self.insert_node(node.right,key,value,index)
-        if index < len(key)-1:
-            self.middl = self.insert_node(node.middle,key,value,index+1)
+        elif index < len(key)-1:
+            node.middle = self.insert_node(node.middle,key,value,index+1)
         else:
             node.value = value
         return node
 
     def get(self,key):
-        node = self.get_node(self.root,key,index)
+        node = self.get_node(self.root,key,0)
         if node is None:
             return -1
         return node.value
+
+    def get_node(self,node,key,index):
+        if node is None:
+            return None
+        c = key[index]
+        if c < node.character:
+            return self.get_node(node.left,key,index)
+        elif c > node.character:
+            return self.get_node(node.right,key,index)
+        elif index < len(key)-1:
+            return self.get_node(node.middle,key,index+1)
+        else:
+            return node
