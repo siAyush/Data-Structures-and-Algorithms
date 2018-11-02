395960ac779f27cd5ffffe13ef5aa8b3de6e2f15
ayush <atulg7516@gmail.com>
2018-11-02 21:30:37 +0530

    bfs

--- Graph/dfs.py
+++ Graph/dfs.py
@@ -1,1 +1,14 @@
+import graph
 
+
+def dfs(graph, start):
+    explored = []
+    queue = [start]
+    while queue:
+        node = queue.pop()
+        if node not in explored:
+            explored.append(node)
+            neighbours = [i for i in graph.outgoing[node].keys()]
+            for neighbour in neighbours:
+                queue.append(neighbour)
+    return explored
